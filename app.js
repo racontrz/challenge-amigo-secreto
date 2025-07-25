dd8af916e26110e8da1799f02b17b1712d4cc79c
racontrz <racontrz@gmail.com>
2025-07-25 18:53:38 -0400

    avances js

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+   let amigo = document.getElementById("amigo").value;
+   console.log(amigo);
+   if (amigo === "") {
+       alert("Por favor, inserte un nombre.");
+   } else {
+       amigos.push(amigo);
+        limpiarInput();
+   }
+}
+
+function limpiarInput() {
+    document.getElementById("amigo").value = "";
+}
+
+function sortearAmigo() {
+    let indice = Math.floor(Math.random()*`${amigos.length}`);
+    let listado= document.getElementById("listaAmigos");
+    let amigo = "";
+    for (let i = 0; i < amigos.length; i++) {
+        amigo = amigos[i];
+        listado.innerHTML = amigo;
+    }
+
+
+    console.log(indice);
+}
+
+
+
