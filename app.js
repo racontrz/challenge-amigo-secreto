f110759f7425530e214c6efbbead59eba722ef75
racontrz <racontrz@gmail.com>
2025-07-26 14:30:54 -0400

    termino de proyecto

--- app.js
+++ app.js
@@ -2,30 +2,34 @@ let amigos = [];
 
 function agregarAmigo() {
    let amigo = document.getElementById("amigo").value;
-   console.log(amigo);
    if (amigo === "") {
        alert("Por favor, inserte un nombre.");
    } else {
        amigos.push(amigo);
-        limpiarInput();
+       limpiarLista();
+       limpiarInput();
+       listaAmigos();
    }
 }
 
 function limpiarInput() {
     document.getElementById("amigo").value = "";
 }
+function limpiarLista(){
+    document.getElementById("listaAmigos").innerHTML = "";
+}
+
+function listaAmigos(){
+    let listado= document.getElementById("listaAmigos");
+    for (let i = 0; i < amigos.length; i++) {
+        listado.innerHTML += `<li> ${amigos[i]} </li>`;
+    }
+}
 
 function sortearAmigo() {
+    let resultado = document.getElementById("resultado");
     let indice = Math.floor(Math.random()*`${amigos.length}`);
-    let listado= document.getElementById("listaAmigos");
-    let amigo = "";
-    for (let i = 0; i < amigos.length; i++) {
-        amigo = amigos[i];
-        listado.innerHTML = amigo;
-    }
-
-
-    console.log(indice);
+    resultado.innerHTML = amigos[indice];
 }
 
 
